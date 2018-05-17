0d9bf6da46df3e3d7858f8188d258d9c11097d1a
DESKTOP-IJD71UV\prith <prithwi3frameslab>
2018-05-17 08:01:16 +0530

    commit3

---
+++ app/src/main/java/com/example/bhargav/myapplication/WelcomeHome/WelcomeHomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.bhargav.myapplication.WelcomeHome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bhargav.myapplication.R;
+
+public class WelcomeHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_home);
+    }
+}
